97d88605c85f11a634d6978cc7d83e363715808e
Nurbekkalil <83503611+Nurbekkalil@users.noreply.github.com>
2021-11-19 16:03:11 +0600

    'Home work for lesson 3 based on es6'

--- twomass.js
+++ twomass.js
@@ -1,27 +1,33 @@
+// Задание 2.2: es6
+//
+// Написать функцию и назвать ее twoMass и передать 2 аргумента array и сравнить их длину
+
+const TwoMass = (mass1, mass2) => {
+    if (mass1.length > mass2.length){
+        console.log('mass1 больше')
+    }
+    else if (mass1.length < mass2.length){
+        console.log('mass1 меньше')
+    }
+}
+TwoMass('toyota', 'lexus')
+
+
+
+
+
 // Задание 2:
 //
 // Написать функцию и назвать ее twoMass и передать 2 аргумента array и сравнить их длину
 
-// function twoMass() {
-//
-// }
 
-// function twoMass(f, a) {
-//   var result = [], // Create a new Array
-//       i;
-//   for (i = 0; i != a.length; i++)
-//     result[i] = f(a[i]);
-//   return result;
+// function twoMass (geektech, makers){
+//     if (geektech.length > makers.length){
+//         console.log('geektech больше')
+//     } else if (makers.length > geektech.length){
+//         console.log('makers больше')
+//     } else{
+//         console.log('равны')
+//     }
 // }
-// twoMass(10,20,)
-
-function twoMass (geektech, makers){
-    if (geektech.length > makers.length){
-        console.log('geektech больше')
-    } else if (makers.length > geektech.length){
-        console.log('makers больше')
-    } else{
-        console.log('равны')
-    }
-}
-twoMass("ghghghfhfgf", "hfgfv")
+// twoMass("ghghghfhfgf", "hfgfv")
